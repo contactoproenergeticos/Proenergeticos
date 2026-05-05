f75f2d710487cd71ee69980624f9150f74084ca2
proenergeticos <contactoproenergeticos@gmail.com>
2026-05-04 22:06:02 -0700

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,7 @@ import { GoogleTagManager } from '@next/third-parties/google';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import dynamic from 'next/dynamic'; // Paso 1: Importar dynamic
-
-// Paso 2: Importar el botón desactivando el renderizado de servidor (SSR)
-const AccessibilityButton = dynamic(() => import('../components/AccessibilityButton'), { 
-  ssr: false 
-});
+import AccessibilityButton from '../components/AccessibilityButton'; // Importación directa
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -26,7 +21,6 @@ export default function RootLayout({
       <GoogleTagManager gtmId="GTM-P53V68D7" />
       <body className={inter.className}>
         {children}
-        {/* Ahora Next.js ignorará este botón durante el build de la página 404 */}
         <AccessibilityButton />
       </body>
     </html>
