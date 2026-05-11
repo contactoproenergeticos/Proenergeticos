bf64c0e930925b9ea6908c9836dccaf68b1305e4
proenergeticos <contactoproenergeticos@gmail.com>
2026-05-11 10:14:43 -0700

    ok

--- app/estaciones/page.tsx
+++ app/estaciones/page.tsx
@@ -95,7 +95,7 @@ export default function EstacionesPage() {
       nombre: 'SANTA IRENE (GSI)', 
       marca: 'Estación de Servicio', 
       direccion: 'Luis Donaldo Colosio Murrieta 14101, Santa Laura, 82136 Mazatlán, Sin.', 
-      mapLink: '#', 
+      mapLink: 'https://maps.google.com/?cid=6818530638675163216&g_mp=CiVnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLkdldFBsYWNl', // Enlace real,     
       imagen: '/images/gasolinera/GSI/gsi3.jpeg', 
       estacionLogo: '/images/logotipos/BLAST.png', 
       servicios: [{ icon: Store, label: 'OXXO', color: 'text-red-600' }, { icon: Clock, label: '24/7', color: 'text-green-600' }, ...serviciosBasicos] 
@@ -104,7 +104,7 @@ export default function EstacionesPage() {
       nombre: 'EL POZOLE (GPO)', 
       marca: 'Estación de Servicio', 
       direccion: 'CARRETERA INTERNACIONAL SUR KM. 60 EL POZOLE, VILLA UNION, Sin.', 
-      mapLink: '#', 
+      mapLink: 'https://maps.google.com/?cid=2912828460837729529&g_mp=CiVnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLkdldFBsYWNl', // Enlace real 
       imagen: '/images/gasolinera/GPO/GPO2.jpg', 
       estacionLogo: '/images/logotipos/GPO.png', 
       servicios: [{ icon: Store, label: 'Kiosko', color: 'text-orange-600' }, { icon: Clock, label: '24/7', color: 'text-green-600' }, ...serviciosBasicos] 
@@ -113,7 +113,7 @@ export default function EstacionesPage() {
       nombre: 'PLANTA DE DISTRIBUCIÓN', 
       marca: 'Centro Logístico', 
       direccion: 'Sur, México 15 1002, Urías, 82070 Mazatlán, Sin.', 
-      mapLink: '#', 
+      mapLink: 'https://maps.google.com/?cid=14017863012502601436&g_mp=CiVnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLkdldFBsYWNl', // Enlace real 
       isFeatured: true, 
       imagen: '/images/gasolinera/PLANTA/Planta3.jpg', 
       estacionLogo: '/images/logotipos/ProEner.png', 
@@ -181,3 +181,4 @@ export default function EstacionesPage() {
     </SiteShell>
   );
 }
+
