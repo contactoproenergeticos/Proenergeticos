be935142e1896cca9fd17606172809b4e3101ce2
iglesia vida nueva <iglesiavidanuevamzt@gmail.com>
2026-06-03 20:30:53 -0700

    Update DieselSlider.tsx

--- components/DieselSlider.tsx
+++ components/DieselSlider.tsx
@@ -32,7 +32,7 @@ const dieselSlides = [
     title: 'NORMATIVA INTERNACIONAL',
     subtitle: 'Garantizamos el cumplimiento estricto de las normas NOM-016-CRE-2016 y los estándares de la US EPA para la reducción de emisiones contaminantes.',
     techData: 'CERTIFICACIÓN US EPA',
-    image: '/images/Camiiones/Camion3.jpg',
+    image: '/images/Camiiones/Camion4.jpg',
     logo: '/images/logotipos/Diesel_cuadro.png',
     accentColor: '#E30613'
   },
